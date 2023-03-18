e72a0b8e008c38411ffe5d34d1d41961faed9cc1
Dorshir <dorshir92@gmail.com>
2023-03-18 16:41:45 +0200

    commit2

--- ADT-main/AdptArray.c
+++ ADT-main/AdptArray.c
@@ -34,20 +34,17 @@ Result SetAdptArrayAt(PAdptArray adptArray, int index, PElement pNewElem) {
 
     PElement *newpElemArr;
 
-    /* if index is more than size of the old array, reallocate the old array to the correct size,
-     * initialize rest elements with null, update Adpt array and size.
+    /* If the given index is higher than the size of the old array -> reallocate the old array memory to the correct size,
+     * initialize new elements with null, update Adpt array and it's size.
     */
     if (index >= adptArray->arrSize) {
         newpElemArr = (PElement *) realloc(adptArray->elemArr, (index + 1) * sizeof(PElement));
         if (newpElemArr == NULL) {
             return FAIL;
         }
-
         for (int i = adptArray->arrSize; i < index + 1; ++i) {
             newpElemArr[i] = NULL;
         }
-
-        // Update array pointer and size
         adptArray->elemArr = newpElemArr;
         adptArray->arrSize = index + 1;
     }
@@ -57,7 +54,7 @@ Result SetAdptArrayAt(PAdptArray adptArray, int index, PElement pNewElem) {
         adptArray->delFunction(adptArray->elemArr[index]);
     }
 
-    // Initialize the array at the correct index in a copy of the new element
+    // Initialize the array at the correct index with a COPY of the new element
     if (pNewElem != NULL) {
         (adptArray->elemArr)[index] = adptArray->copyFunction(pNewElem);
     }
@@ -68,13 +65,13 @@ void DeleteAdptArray(PAdptArray adptArray) {
 
     if (adptArray == NULL) return;
 
-    // Delete and free the pElement's array calling the delFunction
+    // Delete and free elemArr elements, using the delete function
     for (int i = 0; i < adptArray->arrSize; ++i) {
         if ((adptArray->elemArr)[i] == NULL) continue;
         adptArray->delFunction((adptArray->elemArr)[i]);
     }
 
-    // Free the pElements array then the Adpt array
+    // Free elemArr array and then the Adpt array
     free(adptArray->elemArr);
     free(adptArray);
 }
@@ -84,18 +81,22 @@ PElement GetAdptArrayAt(PAdptArray adptArray, int index) {
     if (index > adptArray->arrSize) return NULL;
     if ((adptArray->elemArr[index]) == NULL) return NULL;
 
+    // Initialize a new PElement with a copy of the right element from the array, using the copy function
     PElement elemCopy = adptArray->copyFunction(adptArray->elemArr[index]);
 
     return elemCopy;
 }
 
 int GetAdptArraySize(PAdptArray adptArray) {
+
     if (adptArray == NULL) return -1;
     return adptArray->arrSize;
 }
 
 void PrintDB(PAdptArray adptArray) {
     if (adptArray == NULL) return;
+
+    // Iterating the array and printing, using the print function
     for (int i = 0; i < adptArray->arrSize; ++i) {
         if (adptArray->elemArr[i] == NULL) continue;
         adptArray->printFunction(adptArray->elemArr[i]);
